41b6ab0ac87fb77dd806ce73dffb400c7eb300c2
kiwiyou <kiwiyou@kiwiyou.dev>
2023-12-31 16:00:49 +0900

    Use text/xml in /rss

--- src/pages/rss.ts
+++ src/pages/rss.ts
@@ -1,1 +1,6 @@
-export { GET } from './rss.xml';
+import { GET as rss } from './rss.xml';
+export async function GET() {
+  const response = await rss();
+  response.headers.set('content-type', 'text/xml; charset=utf-8');
+  return response;
+}
