30d8538c34d294072b1556676760ce03372288ae
kiwiyou <kiwiyou@kiwiyou.dev>
2023-12-31 16:09:59 +0900

    Use cloudflare and dynamic generate rss

--- src/pages/rss.ts
+++ src/pages/rss.ts
@@ -1,6 +1,13 @@
 import { GET as rss } from './rss.xml';
+
+export const prerender = false;
+
 export async function GET() {
-  const response = await rss();
-  response.headers.set('content-type', 'text/xml; charset=utf-8');
-  return response;
+  const xml = await rss().then((r) => r.text());
+  return new Response(xml, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/xml; charset=utf-8',
+    },
+  });
 }
