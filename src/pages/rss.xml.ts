c517c91f72189725d5811d92cc573b7e02bc331c
kiwiyou <kiwiyou@kiwiyou.dev>
2023-12-31 15:25:33 +0900

    Try fixing rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -10,6 +10,7 @@ export async function GET() {
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
+    customData: `<language>ko</language><generator>astro</generator><ttl>100</ttl>`,
     items: sortedPosts.map(({ data, slug }) => ({
       link: `posts/${slug}`,
       title: data.title,
