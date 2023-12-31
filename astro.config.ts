23a906d78dd84d578805dbb0d8920e75867e3b89
kiwiyou <kiwiyou@kiwiyou.dev>
2023-12-31 16:49:48 +0900

    Give up naver blog

--- astro.config.ts
+++ astro.config.ts
@@ -6,8 +6,6 @@ import remarkCollapse from 'remark-collapse';
 import sitemap from '@astrojs/sitemap';
 import { SITE } from './src/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
@@ -39,6 +37,5 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: 'where',
-  output: 'hybrid',
-  adapter: cloudflare(),
+  output: 'static',
 });
