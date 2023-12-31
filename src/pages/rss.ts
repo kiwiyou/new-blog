bb6546785aa23e53578f81248420c4a43b7446fa
kiwiyou <kiwiyou@kiwiyou.dev>
2023-12-31 16:22:48 +0900

    Use /rss to generate /rss.xml

--- src/pages/rss.ts
+++ src/pages/rss.ts
@@ -1,13 +1,26 @@
-import { GET as rss } from './rss.xml';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import getSortedPosts from '@utils/getSortedPosts';
+import { SITE } from '@config';
 
 export const prerender = false;
 
 export async function GET() {
-  const xml = await rss().then((r) => r.body);
-  return new Response(xml, {
-    status: 200,
-    headers: {
-      'Content-Type': 'text/xml; charset=utf-8',
-    },
+  const posts = await getCollection('blog');
+  const sortedPosts = getSortedPosts(posts);
+  return rss({
+    title: SITE.title,
+    description: SITE.desc,
+    site: SITE.website,
+    customData: `<language>ko</language><generator>astro</generator><ttl>100</ttl>`,
+    items: sortedPosts.map(({ data, slug }) => ({
+      link: `posts/${slug}`,
+      title: data.title,
+      description: data.description,
+      pubDate: new Date(data.pubDatetime),
+    })),
+  }).then((r) => {
+    r.headers.set('Content-Type', 'text/xml; charset=utf-8');
+    return r;
   });
 }
