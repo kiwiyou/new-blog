869bee9afa1d0e9970ad828282fa348561d6e7b1
kiwiyou <kiwiyou@kiwiyou.dev>
2023-12-31 16:29:42 +0900

    Use fetch

--- src/pages/rss.ts
+++ src/pages/rss.ts
@@ -1,26 +1,13 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import getSortedPosts from '@utils/getSortedPosts';
 import { SITE } from '@config';
 
 export const prerender = false;
 
 export async function GET() {
-  const posts = await getCollection('blog');
-  const sortedPosts = getSortedPosts(posts);
-  return rss({
-    title: SITE.title,
-    description: SITE.desc,
-    site: SITE.website,
-    customData: `<language>ko</language><generator>astro</generator><ttl>100</ttl>`,
-    items: sortedPosts.map(({ data, slug }) => ({
-      link: `posts/${slug}`,
-      title: data.title,
-      description: data.description,
-      pubDate: new Date(data.pubDatetime),
-    })),
-  }).then((r) => {
-    r.headers.set('Content-Type', 'text/xml; charset=utf-8');
-    return r;
+  return fetch(`${SITE.website}/rss.xml`).then((r) => {
+    return new Response(r.body, {
+      headers: {
+        'Content-Type': 'text/xml; charset=utf-8',
+      },
+    });
   });
 }
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,1 +1,21 @@
-export { GET } from './rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import getSortedPosts from '@utils/getSortedPosts';
+import { SITE } from '@config';
+
+export async function GET() {
+  const posts = await getCollection('blog');
+  const sortedPosts = getSortedPosts(posts);
+  return rss({
+    title: SITE.title,
+    description: SITE.desc,
+    site: SITE.website,
+    customData: `<language>ko</language><generator>astro</generator><ttl>100</ttl>`,
+    items: sortedPosts.map(({ data, slug }) => ({
+      link: `posts/${slug}`,
+      title: data.title,
+      description: data.description,
+      pubDate: new Date(data.pubDatetime),
+    })),
+  });
+}
